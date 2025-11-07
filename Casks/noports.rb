2f47b9fd3ec3aa7256678457b6f93b748f67ba53
XavierChanth <xchanthavong@gmail.com>
2025-11-07 11:53:23 -0500

    chore(noports): Update build to v5.13.0

--- Casks/noports.rb
+++ Casks/noports.rb
@@ -1,12 +1,15 @@
 cask "noports" do
   arch arm: "arm64", intel: "x64"
   os macos: "macos", linux: "linux"
-  version 'v5.11.2'
+  version 'v5.13.0'
 
-  macos_arm_sha = "13c384ad74aab82e8d7a81d83abc18d72a76ddaca80b53d32a87cbdd623dd8b9"
-  macos_x64_sha = "2b1619065faac9c7750a17f1b04fe2b2b9b91226de90c1e0554fb0ffc400bf13"
-  linux_arm_sha = "77ec1651b5121068045e898b4d6ce0a1f8b2651bc72d5c1eb5d802aec3d1061b"
-  linux_x64_sha = "8afd8689841861d94bddf122b7053fc1207960f2f58851bfd532bf6022a3525d"
+  macos_arm_sha = '83ca40f17c7bfc0d873f810e41b1cd8ab9fc290ac38a3ef38ecd11f396742df6'
+  macos_x64_sha = '69500b83cce4fa460569dae0e2e5a1ad225d2454a421c823b22379acbcdae223'
+  linux_arm_sha = '896f1d23d96d8de71853f6e2b24e33c4e8c4837bab0c9560932abc5cc7741f7a'
+  linux_x64_sha = '8598e0e07cc82bb22982139477e3310080eb7634d738cd60453c676ea97f66fd'
+
+  # N.B. Be careful about the formatting in the above lines.
+  # They are automatically updated by: .github/workflows/multibuild.yaml
 
   on_macos do
     on_arm do
@@ -27,7 +30,7 @@ cask "noports" do
  
   url "https://github.com/atsign-foundation/noports/releases/download/#{version}/sshnp-#{os}-#{arch}.zip"
   name "noports"
-  desc "Cli binaries for noports (https://noports.com)"
+  desc "Make your devices invisible (https://noports.com)"
   homepage "https://noports.com"
 
   depends_on formula: "ca-certificates"
@@ -44,17 +47,14 @@ cask "noports" do
 
   on_macos do
     service "sshnp/launchd/com.atsign.sshnpd.plist"
+    artifact "sshnp/config/sshnpd.yaml",
+      target: "~/Library/Application Support/Noports/sshnpd.yaml"
   end
 
   on_linux do
     service "sshnp/systemd/sshnpd.service"
-    service "sshnp/systemd/srvd.service"
-    # Note: We don't replace the USER var in systemd services like we do with
-    # universal.sh, but most people don't use brew on linux, so it's fine.
-    artifact "sshnp/systemd/sshnpd.service.d/override.conf",
-        target: "/etc/systemd/system/sshnpd.service.d/override.conf"
-    artifact "sshnp/systemd/srvd.service.d/override.conf",
-        target: "/etc/systemd/system/srvd.service.d/override.conf"
+    artifact "sshnp/config/sshnpd.yaml",
+      target: "/etc/noports/sshnpd.yaml"
   end
 
 
@@ -66,14 +66,10 @@ cask "noports" do
 
   on_linux do
     caveats <<~EOS
-      sshnpd systemd service installed /etc/systemd/system/sshnp.service
-      To edit the configuration, modify the file:
+      NoPorts Daemon installed to: /etc/systemd/system/sshnp.service
+      To edit the configuration:
 
-      vi /etc/systemd/system/sshnp.service.d/override.conf
-
-      After modifying the service, you must reload the configuration:
-
-      systemctl daemon-reload
+      sudo nano /etc/noports/sshnpd.yaml
 
       Once configured, the following commands can be used to manage the service.
       Enable/disable automatic startup:
@@ -81,7 +77,7 @@ cask "noports" do
       systemctl enable sshnpd
       systemctl disable sshnpd
 
-      Start/stop/restart the service:
+      Immediately start/stop/restart the service:
 
       systemctl start sshnpd
       systemctl stop sshnpd
@@ -95,10 +91,10 @@ cask "noports" do
   end
   on_macos do
     caveats <<~EOS
-      sshnpd launchd agent installed ~/Library/LaunchAgents/com.atsign.sshnpd.plist
-      To edit the configuration, modify the file:
+      NoPorts Daemon installed to: ~/Library/Services/com.atsign.sshnpd.plist
+      To edit the configuration:
 
-      xcode ~/Library/LaunchAgents/com.atsign.sshnpd.plist
+      textedit "~/Library/Application Support/NoPorts/sshnpd.yaml"
 
       The service can be enabled or disabled from System Settings under the
       "Login Items" menu.
