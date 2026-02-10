d5458d2e4ecd75581e4c593d4668f90065553f6a
Chris Swan <478926+cpswan@users.noreply.github.com>
2026-02-10 10:40:26 +0000

    feat: MacOS only (for now), and bump to v5.14.5

--- Casks/noports.rb
+++ Casks/noports.rb
@@ -1,36 +1,14 @@
 cask "noports" do
   arch arm: "arm64", intel: "x64"
-  os macos: "macos", linux: "linux"
-  version 'v5.13.0'
 
-  macos_arm_sha = '83ca40f17c7bfc0d873f810e41b1cd8ab9fc290ac38a3ef38ecd11f396742df6'
-  macos_x64_sha = '69500b83cce4fa460569dae0e2e5a1ad225d2454a421c823b22379acbcdae223'
-  linux_arm_sha = '896f1d23d96d8de71853f6e2b24e33c4e8c4837bab0c9560932abc5cc7741f7a'
-  linux_x64_sha = '8598e0e07cc82bb22982139477e3310080eb7634d738cd60453c676ea97f66fd'
+  version 'v5.14.5'
+  sha256 arm:   "892e229e17557aea2eb17318ee11c2ae5955ea84942c847d013e23818c018193",
+         intel: "355b55a71213dbd2f92228b8d89527f9739ae5e2dcd4de3cfec9a423c6933c01"
 
   # N.B. Be careful about the formatting in the above lines.
   # They are automatically updated by: .github/workflows/multibuild.yaml
 
-  ext = OS.mac? ? "zip" : "tgz"
-
-  on_macos do
-    on_arm do
-      sha256 macos_arm_sha
-    end
-    on_intel do
-      sha256 macos_x64_sha
-    end
-  end
-  on_linux do
-    on_arm do
-      sha256 linux_arm_sha
-    end
-    on_intel do
-      sha256 linux_x64_sha
-    end
-  end
- 
-  url "https://github.com/atsign-foundation/noports/releases/download/#{version}/sshnp-#{os}-#{arch}.#{ext}"
+  url "https://github.com/atsign-foundation/noports/releases/download/#{version}/sshnp-#{os}-#{arch}.zip"
   name "noports"
   desc "Make your devices invisible (https://noports.com)"
   homepage "https://noports.com"
@@ -47,18 +25,9 @@ cask "noports" do
   binary "sshnp/npp_file"
   binary "sshnp/npa_file"
 
-  on_macos do
-    service "sshnp/launchd/com.atsign.sshnpd.plist"
-    artifact "sshnp/config/sshnpd.yaml",
-      target: "~/Library/Application Support/Noports/sshnpd.yaml"
-  end
-
-  on_linux do
-    service "sshnp/systemd/sshnpd.service"
-    artifact "sshnp/config/sshnpd.yaml",
-      target: "/etc/noports/sshnpd.yaml"
-  end
-
+  service "sshnp/launchd/com.atsign.sshnpd.plist"
+  artifact "sshnp/config/sshnpd.yaml",
+    target: "~/Library/Application Support/Noports/sshnpd.yaml"
 
   zap do
     trash [
@@ -66,40 +35,13 @@ cask "noports" do
     ]
   end
 
-  on_linux do
-    caveats <<~EOS
-      NoPorts Daemon installed to: /etc/systemd/system/sshnp.service
-      To edit the configuration:
+  caveats <<~EOS
+    NoPorts Daemon installed to: ~/Library/Services/com.atsign.sshnpd.plist
+    To edit the configuration:
 
-      sudo nano /etc/noports/sshnpd.yaml
+    textedit "~/Library/Application Support/NoPorts/sshnpd.yaml"
 
-      Once configured, the following commands can be used to manage the service.
-      Enable/disable automatic startup:
-
-      systemctl enable sshnpd
-      systemctl disable sshnpd
-
-      Immediately start/stop/restart the service:
-
-      systemctl start sshnpd
-      systemctl stop sshnpd
-      systemctl restart sshnpd
-
-      View status/logs:
-
-      systemctl status sshnpd
-      journalctl -u sshnpd
-    EOS
-  end
-  on_macos do
-    caveats <<~EOS
-      NoPorts Daemon installed to: ~/Library/Services/com.atsign.sshnpd.plist
-      To edit the configuration:
-
-      textedit "~/Library/Application Support/NoPorts/sshnpd.yaml"
-
-      The service can be enabled or disabled from System Settings under the
-      "Login Items" menu.
-    EOS
-  end
+    The service can be enabled or disabled from System Settings under the
+    "Login Items" menu.
+  EOS
 end
